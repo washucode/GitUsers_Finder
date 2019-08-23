07a1dd06ff2480f46dd093c90449558d1ae7f410
washucode <estherw8525@gmail.com>
2019-08-23 15:37:00 +0300

    add git-search-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitSearchNavComponent } from './git-search-nav/git-search-nav.component';
 import { GitSearchComponent } from './git-search/git-search.component';
+import { GitDisplaySearchComponent } from './git-display-search/git-display-search.component';
+import { GitSearchFormComponent } from './git-search-form/git-search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GitSearchNavComponent,
-    GitSearchComponent
+    GitSearchComponent,
+    GitDisplaySearchComponent,
+    GitSearchFormComponent
   ],
   imports: [
     BrowserModule,

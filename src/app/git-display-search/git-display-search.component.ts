bc3755d4992d3802f96592c9644400dea45c0e99
washucode <estherw8525@gmail.com>
2019-08-23 15:35:54 +0300

    add git-display-search component

---
+++ src/app/git-display-search/git-display-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-git-display-search',
+  templateUrl: './git-display-search.component.html',
+  styleUrls: ['./git-display-search.component.css']
+})
+export class GitDisplaySearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

a1de28d4c9da5e40d2c456675d7726cc2ed1fa08
washucode <estherw8525@gmail.com>
2019-08-25 17:15:38 +0300

    add service to search repos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GitSearchComponent } from './git-search/git-search.component';
 import { GitDisplaySearchComponent }from './git-display-search/git-display-search.component';
+import { DisplayReposComponent} from './display-repos/display-repos.component';
 
 const routes: Routes = [
   {path:'home', component:GitSearchComponent},
   {path:'display_results', component:GitDisplaySearchComponent},
+  {path:'displayRepos', component:DisplayReposComponent},
   {'pathMatch':'full','redirectTo':'home','path':''},
 
 ];
--- src/app/display-repos/display-repos.component.ts
+++ src/app/display-repos/display-repos.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
-@Component({
+import {UserRepo}  from '../user-repo';
+import {GitSearchHttpService} from '../git-search-http.service';
+ @Component({
   selector: 'app-display-repos',
   templateUrl: './display-repos.component.html',
   styleUrls: ['./display-repos.component.css']
 })
 export class DisplayReposComponent implements OnInit {
-
-  constructor() { }
+  repo : UserRepo;
+  searchTerm : string;
+  constructor(public searchService:GitSearchHttpService) { }
 
   ngOnInit() {
+  }
+  parentGetResult(searchTerm){
+    this.searchService.getRepos(searchTerm).then(
+      ()=>{
+        this.repo = this.searchService.repo;
+      },
+      (error)=>{
+        console.log(error);
+      }
+    )
   }
 
 }
